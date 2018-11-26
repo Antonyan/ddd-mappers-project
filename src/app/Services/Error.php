7823faed2e41ec5b3e71e0d49679f35f9ef6a14f
Mr <andriy.osf@gmail.com>
2018-11-26 15:55:58 +0000

    Improved error handler

--- src/app/Services/Error.php
+++ src/app/Services/Error.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Services;
 
+use Infrastructure\Application;
+use Infrastructure\Exceptions\HttpExceptionInterface;
 use Infrastructure\Services\BaseService;
 use Infrastructure\Services\ErrorHandlerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -8,12 +10,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Error extends BaseService implements ErrorHandlerInterface
 {
-    public function handle(\Infrastructure\Exceptions\HttpExceptionInterface $exception): Response
+    public function handle(HttpExceptionInterface $exception): Response
     {
         return new JsonResponse([
-            'message' => $exception->getMessage(),
+            'message' => $this->getEnvDecoratedExceptionMessage($exception),
             'errorCode' => $exception->getErrorCode(),
             'errors' => $exception->getBody()
         ], $exception->getStatusCode(), $exception->getHeaders());
+    }
+
+    private function getEnvDecoratedExceptionMessage(HttpExceptionInterface $exception)
+    {
+        if ($exception->getStatusCode() >= Response::HTTP_INTERNAL_SERVER_ERROR && getenv('ENV') == Application::ENV_PROD) {
+            return 'Internal server error';
+        }
+
+        return $exception->getMessage();
     }
 }
